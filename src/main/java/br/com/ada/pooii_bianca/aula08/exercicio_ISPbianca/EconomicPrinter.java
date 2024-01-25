8d1b639314e98ce84b6b9dfa1f389c70b8ef8f69
Bianca Fonseca <b.bianca.fonseca@gmail.com>
2024-01-24 21:23:04 -0300

    Enviando os arquivos completos.

--- src/main/java/br/com/ada/pooii_bianca/aula08/exercicio_ISPbianca/EconomicPrinter.java
+++ src/main/java/br/com/ada/pooii_bianca/aula08/exercicio_ISPbianca/EconomicPrinter.java
@@ -1,13 +1,25 @@
 package br.com.ada.pooii_bianca.aula08.exercicio_ISPbianca;
 
 //essa classe irá implementar somente os métodos que REALMENTE precisa
-public class EconomicPrinter implements Print, Scan{
+public class EconomicPrinter implements MultifunctionPrinter{
+
+    @Override
+    public void copy() {
+
+    }
+
+    @Override
+    public void fax() {
+
+    }
 
     @Override
     public void print() {
+
     }
 
     @Override
     public void scan() {
+
     }
 }
